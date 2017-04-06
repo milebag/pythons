ee42b44dfb7734f9a19fa6d7e3bafd692fd17bff
milebag <andt@eyemile.com>
2017-04-06 07:36:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,23 +5,23 @@ HOST = ''   # Symbolic name meaning all available interfaces
 PORT = 80 # Arbitrary non-privileged port
  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print 'Socket created'
+print ('Socket created')
  
 try:
     s.bind((HOST, PORT))
 except socket.error , msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    print ('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
     sys.exit()
      
-print 'Socket bind complete'
+print ('Socket bind complete')
  
 s.listen(10)
-print 'Socket now listening'
+print ('Socket now listening')
  
 #wait to accept a connection - blocking call
 conn, addr = s.accept()
  
-print 'Connected with ' + addr[0] + ':' + str(addr[1])
+print ('Connected with ' + addr[0] + ':' + str(addr[1]))
  
 #now keep talking with the client
 data = conn.recv(1024)

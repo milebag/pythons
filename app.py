0f538d0b0ef4f6c63cfcdfda6d1c709776886f68
milebag <andt@eyemile.com>
2017-04-06 07:33:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
-
-import SimpleHTTPServer 
-SimpleHTTPServer.test()
-
+import socket
+import sys
+ 
+HOST = ''   # Symbolic name meaning all available interfaces
+PORT = 80 # Arbitrary non-privileged port
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+ 
+try:
+    s.bind((HOST, PORT))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+     
+print 'Socket bind complete'
+ 
+s.listen(10)
+print 'Socket now listening'
+ 
+#wait to accept a connection - blocking call
+conn, addr = s.accept()
+ 
+print 'Connected with ' + addr[0] + ':' + str(addr[1])
+ 
+#now keep talking with the client
+data = conn.recv(1024)
+conn.sendall(data)
+ 
+conn.close()
+s.close() 
 
 print("hello")
